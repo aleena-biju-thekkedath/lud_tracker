e44a5b50dc4089f86fc1ae8047877922b6bedf25
aleena-biju-thekkedath <aleena.biju@bds.christuniversity.in>
2023-08-17 18:47:17 +0530

    Renaming project to lud_tracker

--- lud_tracker/settings.py
+++ lud_tracker/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "pro_track.urls"
+ROOT_URLCONF = "lud_tracker.urls"
 
 TEMPLATES = [
     {
@@ -72,7 +72,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "pro_track.wsgi.application"
+WSGI_APPLICATION = "lud_tracker.wsgi.application"
 
 
 # Database
--- lud_tracker/wsgi.py
+++ lud_tracker/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pro_track.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lud_tracker.settings")
 
 application = get_wsgi_application()
