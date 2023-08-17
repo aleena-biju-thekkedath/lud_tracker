acfaa1eb26c9733cef52cadf87a5df6fba360148
aleena-biju-thekkedath <aleena.biju@bds.christuniversity.in>
2023-08-17 19:23:29 +0530

    adding homepage url for member

--- lud_tracker/settings.py
+++ lud_tracker/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = "lud_tracker.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
