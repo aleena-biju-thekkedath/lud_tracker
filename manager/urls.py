5ca18887afa2e9219bba1a58294ead7a4e71f201
JOE1904 <joanjob1904@gmail.com>
2023-08-22 11:24:45 +0530

    Created Team HTML page and URL for it

--- manager/urls.py
+++ manager/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.home_manager,name='manager-home')
+    path("",views.home_manager,name='manager-home'),
+    path("team/",views.create_team, name = 'manager-create'),
 ]
