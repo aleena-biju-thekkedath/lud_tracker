99ce0f9f84d6ce67e287e6d244e0e08058bcf6d4
aleena-biju-thekkedath <aleena.biju@bds.christuniversity.in>
2023-08-22 14:28:48 +0530

    login page

--- lud_tracker/urls.py
+++ lud_tracker/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("members/",include('member.urls')),
+    path("",include('member.urls')),
     path("manager/",include('manager.urls')),
-    path("leads/",include('lead.urls'))
+    path("leads/",include('lead.urls')),
 ]
--- member/views.py
+++ member/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home_member(request):
     return render(request,'member/member.html')
+
+def login_page(request):
+    return render(request,'member/login.html')
