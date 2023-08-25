2da1e40c1735098165c2dab022b77a9014019dfa
JOE1904 <joanjob1904@gmail.com>
2023-08-25 12:20:53 +0530

    Created Member Registration Page for Manager/Superuser

--- manager/views.py
+++ manager/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+from .forms import CreateUserForm
+
+
 def home_manager(request):
+    # user = User.objects.create()
     return render(request,'manager/manager.html')
 
 def create_team(request):
     return render(request,'manager/create_team.html')
 
-def calendar(request):
-    return render(request,'manager/calendar.html')
+def register(request):
+    form = CreateUserForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
 
+    context = {"form": form}
+    return render(request,'manager/register.html',context)
+      
 
