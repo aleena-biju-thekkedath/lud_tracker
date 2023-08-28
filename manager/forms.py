f840312eeac8d27dc45b06c343bfa09db6efe046
aleena-biju-thekkedath <aleena.biju@bds.christuniversity.in>
2023-08-28 16:17:23 +0530

    dd

--- manager/forms.py
+++ manager/forms.py
@@ -7,11 +7,9 @@ from django.contrib.auth.models import User
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ["username","email","password","password2"]
+        fields = ["username","email"]
 
         widgets = {
             "username": forms.TextInput(attrs= {"class": "form-control","placeholder":"UserName"}),
             "email": forms.TextInput(attrs= {"class": "form-control","placeholder":"Email Id"}),
-            "password" : forms.PasswordInput(attrs= {"class": "form-control","placeholder":"Password"}),
-            "password2": forms.PasswordInput(attrs= {"class": "form-control","placeholder":"Confirm Password"}),
-        }
+            }
