238cc2fbe9415fceb8dc72f737455932f77ed8c2
Alee <123159057+aleena-biju-thekkedath@users.noreply.github.com>
2023-08-28 07:08:10 +0000

    Joan's page

--- manager/views.py
+++ manager/views.py
@@ -1,5 +1,5 @@
 import datetime
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from datetime import datetime
 from django.shortcuts import render
@@ -12,19 +12,20 @@ from lud_tracker import settings
 
 
 # Create your views here.
-# from .forms import CreateUserForm
+from .forms import CreateUserForm
 
 
 def home_manager(request):
     # user = User.objects.create()
-    return render(request,'manager/manager.html')
+    return render(request, 'manager/manager.html')
+
 
 def create_team(request):
-    return render(request,'manager/create_team.html')
- 
+    return render(request, 'manager/create_team.html')
+
 
 def process_uploaded_csv(request):
-   if request.POST:
+    if request.POST:
         # if request.user.is_superuser:
         uploaded_file = request.FILES["file"]
         filepath = "files/" + uploaded_file.name
@@ -38,43 +39,23 @@ def process_uploaded_csv(request):
             if is_first_row:
                 is_first_row = False
                 continue  # Skip the first row
-       
+
             fields = line.split(",")
             print(fields[0])
-            user = User(username=fields[0],email=fields[1],password= make_password(fields[2]))
+            user = User(username=fields[0], email=fields[1],
+                        password=make_password(fields[2]))
             user.save()
-        
 
         return redirect('manager-home')
-   
-   else: 
-       return render(request,"manager/record_upload.html")
+    else:
+        return render(request, "manager/register.html")
+
 
 def register(request):
-    # user = User.objects.create()
-    return render(request,'manager/register.html')
-   
-
-
-
-
-
-
-
-
-
-    
-    # form = CreateUserForm()
-    # if request.method == "POST":
-    #     form = UserCreationForm(request.POST)
-    #     if form.is_valid():
-    #         form.save()
-    # context = {"form": form}
-    # return render(request,'manager/register.html',context)
-    # In your app's views.py
-
-  
-
-
-      
-
+    form = CreateUserForm()
+    if request.POST:
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {"form": form}
+    return render(request, 'manager/register.html', context)
